f76641c25b1e0ab81243f084e695b7d297355eca
Matt Ramotar <matt.ramotar@jhu.edu>
2021-01-18 12:31:59 -0500

    Update workout set model

--- api/models/WorkoutSet.js
+++ api/models/WorkoutSet.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose');
-mongoose.set('useCreateIndex', true);
+const mongoose = require("mongoose");
+mongoose.set("useCreateIndex", true);
 
 const Schema = mongoose.Schema;
 
 const WorkoutSetSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  user: { type: Schema.Types.ObjectId, ref: "User" },
   reps: { type: Number },
   pounds: { type: Number },
-  workout: { type: Schema.Types.ObjectId, ref: 'Workout' },
-  exercise: { type: Schema.Types.ObjectId, ref: 'Exercise' },
+  workout: { type: Schema.Types.ObjectId, ref: "Workout" },
+  exercise: { type: Schema.Types.ObjectId, ref: "Exercise" },
   timestamp: { type: Date, default: Date.now() },
   personalRecords: { type: Schema.Types.Mixed },
+  gainedXP: { type: Number },
 });
 
-module.exports = mongoose.model('WorkoutSet', WorkoutSetSchema);
+module.exports = mongoose.model("WorkoutSet", WorkoutSetSchema);
