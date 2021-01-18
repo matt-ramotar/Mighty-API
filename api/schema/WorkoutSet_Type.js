56339a2c7c029408e6105275d3cc30ca1c6dbe90
Matt Ramotar <matt.ramotar@jhu.edu>
2021-01-18 16:48:31 -0500

    Fix lint errors

--- api/schema/WorkoutSet_Type.js
+++ api/schema/WorkoutSet_Type.js
@@ -1,25 +1,12 @@
 const mongoose = require("mongoose");
 const graphql = require("graphql");
 
-const {
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLID,
-  GraphQLList,
-  GraphQLInt,
-  GraphQLBoolean,
-} = graphql;
-const {
-  GraphQLDate,
-  GraphQLTime,
-  GraphQLDateTime,
-} = require("graphql-iso-date");
+const { GraphQLObjectType, GraphQLID, GraphQLInt } = graphql;
+const { GraphQLDateTime } = require("graphql-iso-date");
 const { GraphQLJSON } = require("graphql-type-json");
 
-const User_Type = require("./User_Type");
 const Exercise_Type = require("./Exercise_Type");
-const WorkoutSet = mongoose.model("WorkoutSet");
-const User = mongoose.model("User");
+
 const Exercise = mongoose.model("Exercise");
 
 const WorkoutSet_Type = new GraphQLObjectType({
@@ -29,22 +16,13 @@ const WorkoutSet_Type = new GraphQLObjectType({
     reps: { type: GraphQLInt },
     pounds: { type: GraphQLInt },
 
-    // user: {
-    //   type: User_Type,
-
-    //   resolve(parentValue) {
-    //     return User.findById(parentValue.user)
-    //       .then(user => user)
-    //       .catch(err => null);
-    //   },
-    // },
     exercise: {
       type: Exercise_Type,
 
       resolve(parentValue) {
         return Exercise.findById(parentValue.exercise)
           .then((exercise) => exercise)
-          .catch((err) => null);
+          .catch(() => null);
       },
     },
 
